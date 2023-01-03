dde0a8bf6c1988b249114b766e8969b5ad44ec53
Aidymouse <aidymouse4891@gmail.com>
2023-01-04 01:23:11 +1300

    gh page

--- gh-pages.js
+++ gh-pages.js
@@ -7,7 +7,7 @@ ghpages.publish(
         repo: 'https://github.com/Aidymouse/map-numberer.git', // Update to point to your repository
         user: {
             name: 'Aidymouse', // update to use your name
-            email: 'aidan@thewizardscat.co.nz', // Update to use your email
+            email: 'aidymouse4891@gmail.com', // Update to use your email
         },
         message: 'GH Pages Script',
     },
